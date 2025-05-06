7ecf0d583a6f67bd19c842ef35008cb1bf5802d0
Utop-ia <tommasopaganini@gmail.com>
2025-05-06 12:27:41 +0200

    changes

--- sketch.js
+++ sketch.js
@@ -3,14 +3,15 @@ let copie = 30;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
-  //creo il mio dataset
+  //creo il mio dataset (lista di oggetti)
   for (let i = 0; i < 100; i++) {
     let cubo = {
-      x: random(-1000, 1000),
-      y: random(-1000, 1000),
-      z: random(-1000, 1000),
+      x: random(-2000, 2000),
+      y: random(-2000, 2000),
+      z: random(-2000, 2000),
       color: random(["green", "blue", "red", "yellow", "purple", "orange"]),
-      size: random(50, 500),
+      size: random(50, 750),
+      strokesize: random(5, 50),
     };
     cubi.push(cubo);
   }
@@ -19,11 +20,12 @@ function setup() {
 function draw() {
   background(220);
   orbitControl();
-  // visualizzo il mio dataset
+
+  // visualizzo il mio dataset (i miei oggetti)
   for (let cubo of cubi) {
     push();
     translate(cubo.x, cubo.y, cubo.z);
-    strokeWeight(5);
+    strokeWeight(cubo.strokesize);
     stroke(cubo.color);
     noFill();
     box(cubo.size);
