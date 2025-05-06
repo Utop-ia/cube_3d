f205ca2a416daf2b786cea84c7678026943c0601
Utop-ia <tommasopaganini@gmail.com>
2025-05-06 12:28:40 +0200

    A

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
       y: random(-2000, 2000),
       z: random(-2000, 2000),
       color: random(["green", "blue", "red", "yellow", "purple", "orange"]),
-      size: random(50, 750),
+      size: random(50, 500),
       strokesize: random(5, 50),
     };
     cubi.push(cubo);
