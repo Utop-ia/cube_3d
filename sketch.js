e6b6155bfff6452020c61dcee88c6e189b8b1a38
Utop-ia <tommasopaganini@gmail.com>
2025-05-06 12:08:22 +0200

    some changes

--- sketch.js
+++ sketch.js
@@ -1,15 +1,27 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, "webgl");
 }
 
 function draw() {
   background(220);
+  orbitControl();
   const s = 100;
+  //cubi che si moltiplicano in modo casuale
+  for (let i = 0; i < 100; i++) {
+    let x = random(-1000, 1000);
+    let y = random(-1000, 1000);
+    let z = random(-1000, 1000);
 
-  rect(0, 0, s, s);
-  rect(0, height - s, s, s);
-  rect(width - s, 0, s, s);
-  rect(width - s, height - s, s, s);
+    let color = random(["green", "blue", "red", "yellow", "purple", "orange"]);
+    let size = random(50, 500);
+    push();
+    translate(x, y, z);
+    strokeWeight(5);
+    stroke(color);
+    noFill();
+    box(size);
+    pop();
+  }
 }
 
 function windowResized() {
